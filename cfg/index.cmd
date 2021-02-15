1cfae21cbb005ebc68b969367e448ca4df9b76e3
IanKing <72170170+Epicminer256@users.noreply.github.com>
2021-02-15 14:36:25 -0600

    help support params

--- cfg/index.cmd
+++ cfg/index.cmd
@@ -1,22 +1,22 @@
 ::You might need to comment/uncomment file types 
-::(example: if there isnt at least one .lnk file in the bin folder, it will pull a ìFile Not Foundî error)
+::(example: if there isnt at least one .lnk file in the bin folder, it will pull a ‚ÄúFile Not Found‚Äù error)
 ::
 ::So if you are not dealing with links, Comment out the line below (searches for .LNK files)
 
-
+::DO NOT CHANGE THIS LINE
+for /F %%G IN ("%exec%") do set "exec1=%%G"
 
 ::searches for .BAT files in the bin dir
-for /f "delims=" %%a in ('dir /B %startdir%\bin\*.bat') do if "%exec%.bat" == "%%a" "%startdir%bin\%%a"
+for /f "delims=" %%a in ('dir /B %startdir%\bin\*.bat') do if "%exec1%.bat" == "%%a" %startdir%bin\%exec%
 
 
 ::searches for .LNK files in the bin dir
-for /f "delims=" %%a in ('dir /B %startdir%\bin\*.lnk') do if "%exec%.lnk" == "%%a" "%startdir%bin\%%a"
-
+for /f "delims=" %%a in ('dir /B %startdir%\bin\*.lnk') do if "%exec1%.lnk" == "%%a" %startdir%bin\%exec%
 
 
 ::searches for .EXE files in the bin dir
 ::Uncomment the text bellow for EXE file support
-::for /f "delims=" %%a in ('dir /B %startdir%\bin\*.exe') do if "%exec%.exe" == "%%a" "%startdir%bin\%%a"
+::for /f "delims=" %%a in ('dir /B %startdir%\bin\*.exe') do if "%exec1%.exe" == "%%a" %startdir%bin\%exec%
 
 
 
