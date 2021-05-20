6981faa9759a0915d4d1570354847e44b6eefa36
IanKing <72170170+Epicminer256@users.noreply.github.com>
2021-05-19 18:39:30 -0600

    Pathing Error Fix Again

--- cfg/index.cmd
+++ cfg/index.cmd
@@ -7,24 +7,21 @@
 for /F %%G IN ("%exec%") do set "exec1=%%G"
 
 ::searches for .BAT files in the bin dir
-for /f "delims=" %%a in ('dir /B %startdir%\bin\*.bat') do if "%exec1%.bat" == "%%a" %startdir%bin\%exec%
+for /f "delims=" %%a in ('dir /B "%startdir%bin\*.bat"') do if "%exec1%.bat" == "%%a" "%startdir%bin\%exec%"
 
 
 ::searches for .LNK files in the bin dir
-for /f "delims=" %%a in ('dir /B %startdir%\bin\*.lnk') do if "%exec1%.lnk" == "%%a" %startdir%bin\%exec%
+for /f "delims=" %%a in ('dir /B "%startdir%bin\*.lnk"') do if "%exec1%.lnk" == "%%a" "%startdir%bin\%exec%"
 
 
 ::searches for .EXE files in the bin dir
 ::Uncomment the text bellow for EXE file support
-::for /f "delims=" %%a in ('dir /B %startdir%\bin\*.exe') do if "%exec1%.exe" == "%%a" %startdir%bin\%exec%
-
-
-
+::for /f "delims=" %%a in ("dir /B "%startdir%bin\*.exe"') do if "%exec1%.exe" == "%%a" "%startdir%bin\%exec%"
 
 
 :Dont remove this command
 ::searches for exact same named files in the bin dir
-for /f "delims=" %%a in ('dir /B %startdir%\bin\*') do if "%exec%" == "%%a" "%startdir%bin\%%a"
+for /f "delims=" %%a in ('dir /B "%startdir%bin\"*') do if "%exec%" == "%%a" "%startdir%bin\%%a"
 
 %exec%
 :end
