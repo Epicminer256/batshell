25954fa42ff960df993d3db3d16bafbcdb1aec28
IanKing <72170170+Epicminer256@users.noreply.github.com>
2021-05-19 19:12:28 -0600

    Made a Version, Patch, and Provider Tag

--- shell.bat
+++ shell.bat
@@ -5,6 +5,14 @@
 
 @echo off
 cls
+
+::This is for compatability purpouses so you can make a program
+::that will exit if batShell is outdated or overdated
+::If you make your own verson of batShell, change the provider tag
+set batshellver=1.1
+set patchnum=1
+set provider=batShell
+
 set startdir="%HOMEDRIVE%""%~p0"
 set startshell="%HOMEDRIVE%""%0"
 set startdir=%startdir:"=%
