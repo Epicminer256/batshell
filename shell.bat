6785dd65364e2ddf2e3ddcf3d5340f8b68ff46f2
IanKing <72170170+Epicminer256@users.noreply.github.com>
2021-05-21 10:34:51 -0600

    Made a title

--- shell.bat
+++ shell.bat
@@ -5,12 +5,12 @@
 
 @echo off
 cls
-
+title Loading...
 ::This is for compatability purpouses so you can make a program
 ::that will exit if batShell is outdated or overdated
 ::If you make your own verson of batShell, change the provider tag
 set batshellver=1.1
-set patchnum=2
+set patchnum=1
 set provider=batShell
 
 set startdir="%HOMEDRIVE%""%~p0"
@@ -19,13 +19,16 @@ set startdir=%startdir:"=%
 set startshell=%startshell:"=%
 
 call "%~p0\cfg\autoexec.cmd"
+title batShell
 
 :start
 set exec=
 echo ^#---%cd%--^>
 set /p exec="#------> "
+title batShell =%exec%=
 call "%~p0\cfg\index.cmd"
 echo.
+title batShell -%exec%-
 goto start
 
 
