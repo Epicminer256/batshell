82453f878c46069bc551940e8815cdfec20c31ac
IanKing <72170170+Epicminer256@users.noreply.github.com>
2021-06-13 19:49:08 -0700

    Plz Read Reverting Changes
    If you put this in a folder that has a space, this program will not work. Will be resolved later

--- shell.bat
+++ shell.bat
@@ -5,30 +5,16 @@
 
 @echo off
 cls
-title Loading...
-::This is for compatability purpouses so you can make a program
-::that will exit if batShell is outdated or overdated
-::If you make your own verson of batShell, change the provider tag
-set batshellver=1.1
-set patchnum=1
-set provider=batShell
-
-set startdir="%HOMEDRIVE%""%~p0"
+set startdir="%~p0"
 set startshell="%0"
-set startdir=%startdir:"=%
-set startshell=%startshell:"=%
-
 call "%~p0\cfg\autoexec.cmd"
-title batShell
 
 :start
 set exec=
 echo ^#---%cd%--^>
 set /p exec="#------> "
-title batShell =%exec%=
 call "%~p0\cfg\index.cmd"
 echo.
-title batShell -%exec%-
 goto start
 
 
