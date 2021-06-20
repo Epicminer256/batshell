225187e8ec77c533f1be8bb1db737ccc5ec418b4
IanKing <72170170+Epicminer256@users.noreply.github.com>
2021-06-20 05:44:52 -0700

    "Help" will allow to see only one app's help page

--- bin/help.bat
+++ bin/help.bat
@@ -1,2 +1,13 @@
+if "%1" == "" goto helpall
+goto helpapp
+
+:helpall
 echo.
 for /f "delims=" %%a in ('dir /B "%startdir%\cfg\manual\*.man"') do more "%startdir%\cfg\manual\%%a" && echo.
+goto done
+
+:helpapp
+more "%startdir%\cfg\manual\%1.man"
+goto done
+
+:done
