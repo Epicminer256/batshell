4f3ddc5f14806addd443e15ae10b5fee901a362a
IanKing <72170170+Epicminer256@users.noreply.github.com>
2021-06-20 21:33:12 -0700

    Optimized the indexing
    instead of trying to see every file inside the \bin\ folder 3 times I just do a regular file check.

--- cfg/index.cmd
+++ cfg/index.cmd
@@ -1,31 +1,25 @@
-::You might need to comment/uncomment file types 
-::(example: if there isnt at least one .lnk file in the bin folder, it will pull a “File Not Found” error)
-
 ::grabs first line of input
 for /F %%G IN ("%exec%") do set "exec1=%%G"
 
-
 ::searches for .BAT files in the bin dir
-for /f "delims=" %%a in ('dir /B "%startdir%\bin\"') do if "%exec1%.bat" == "%%a" "%startdir%bin\"%exec%
-
-
-::searches for .LNK files in the bin dir
-::UNCOMMENT NEXT LINE IF YOU HAVE ANY LINK FILES, COMMENT IT IF YOU DONT HAVE ANY
-for /f "delims=" %%a in ('dir /B "%startdir%\bin\"') do if "%exec1%.lnk" == "%%a" "%startdir%bin\"%exec%
-
-
-::searches for .EXE files in the bin dir
-::UNCOMMENT NEXT LINE  IF YOU HAVE EXE FILES, COMMENT IT IF YOU DONT HAVE ANY
-for /f "delims=" %%a in ('dir /B "%startdir%\bin\"') do if "%exec1%.exe" == "%%a" "%startdir%bin\"%exec%
-
-
-::searches for .CMD files in the bin dir
-for /f "delims=" %%a in ('dir /B "%startdir%\bin\"') do if "%exec1%.exe" == "%%a" "%startdir%bin\"%exec%
-
+if exist "%startdir%\bin\%exec1%.bat" goto execapp
+if exist "%startdir%\bin\%exec1%.exe" goto execapp
+if exist "%startdir%\bin\%exec1%.cmd" goto execapp
+if exist "%startdir%\bin\%exec1%.lnk" goto execapp
+if exist "%startdir%\bin\%exec1%" goto execapp
 
 :Dont remove this command
 ::searches for exact same named files in the bin dir
 for /f "delims=" %%a in ('dir /B %startdir%\bin\*') do if "%exec%" == "%%a" "%startdir%bin\%%a"
+goto execprog
 
+:execapp
+"%startdir%bin\"%exec%
+goto end
+
+:execprog
 %exec%
+goto end
+
+
 :end
